8b44744f4d295bd56b990f5a56f01809b86c60f3
devshum <57689318+devshum@users.noreply.github.com>
2020-03-19 14:49:17 +0200

    .

---
+++ quiz-js-app-flexbox-mvc-es6-modules/src/js/views/base.js
@@ -0,0 +1,26 @@
+export const elements = {
+    // CONTAINERS
+    resultsContainer: document.querySelector('.container__results'),
+    mainContainer: document.querySelector('.container__main'),
+    introContainer: document.querySelector('.container__intro'),
+    btnBox: document.querySelector('.footer__btn-box'),
+    // LABELS
+    introQuestionLabel: document.querySelector('.container__paragraph'),
+    finishSummaryLabel: document.querySelector('.header__summary'),
+    footerLabel: document.querySelector('.footer__label'),
+    correctLabel: document.querySelector('.correct'),
+    wrongLabel: document.querySelector('.wrong'),
+    // BUTTONS
+    btnResult: document.querySelector('.btn--result'),
+    btnStart: document.querySelector('.btn--start'),
+    btnCheck: document.querySelector('.btn--check'),
+    btnRetry: document.querySelector('.btn--retry'),
+    btnSkip: document.querySelector('.btn--skip'),
+    btnNext: document.querySelector('.btn--next'),
+    btnLast: document.querySelector('.btn--last'),
+    // LAYOUT
+    header: document.querySelector('.header'),
+    footer: document.querySelector('.footer'),
+    body: document.querySelector('body')
+};
+
