9b04a7a90c50f0811ba5324cd00b921227179714
devshum <57689318+devshum@users.noreply.github.com>
2020-03-19 14:59:12 +0200

    .

--- quiz-js-app-flexbox-mvc-es6-modules/src/js/models/Question.js
+++
@@ -1,9 +0,0 @@
-export default class Question {
-    constructor(id, question, answers, correct, tips) {
-        this.id = id;
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-        this.tips = tips;
-    }
-};
