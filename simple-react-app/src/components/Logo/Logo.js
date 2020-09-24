2453e18c6686ea7f0409f3c66247ba6d75bd65fc
devshum <57689318+devshum@users.noreply.github.com>
2020-09-24 15:01:31 +0300

    .

--- simple-react-app/src/components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import img from '../../assets/images/logo.png';
-import styles from './Logo.module.css'
-
-const logo = () => (
-    <img className={styles["Logo"]} src={img} alt="NP Shopping logo" />
-);
-
-export default logo;
